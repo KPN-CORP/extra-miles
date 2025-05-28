05ef174cad0305c737740477815877d46b401f9b
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2025-05-28 09:52:04 +0700

    add Extra Miles User Features

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'remixicon/fonts/remixicon.css';
 import '../css/global.css';
-import AppWrapper from './app';
+import AppWrapper from './app.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,41 @@ import path from "path";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/main.jsx'],
+            input: [
+                "resources/css/app.css", 
+                "resources/js/main.jsx",
+            ],
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                globals: {
+                //    jquery: 'window.jQuery',
+                   jquery: 'window.$'
+                }
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            $: "jQuery",
+            Swal: path.resolve(__dirname, "node_modules/sweetalert2"),
+            select2: path.resolve(__dirname, "node_modules/select2"),
+        },
+    },
+    optimizeDeps: {
+        include: ['select2'],
+    },
+    css: {
+        preprocessorOptions: {
+            css: {
+                importLoaders: 1,
+                modules: true,
+            },
+        },
+    },
 });
