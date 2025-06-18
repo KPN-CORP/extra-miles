21589dfa917892dcf8299e568a22fbd653603bff
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2025-06-18 11:03:19 +0700

    optimize social media

--- resources/js/users/components/Helper/instagramHelper.jsx
+++ resources/js/users/components/Helper/instagramHelper.jsx
@@ -4,20 +4,20 @@ const InstagramPlayer = ({ postId }) => {
   return (
     <div className="aspect-[9/16] w-full rounded shadow-md overflow-hidden bg-black relative group">
       <InstagramEmbed
-        url={`https://www.instagram.com/p/${postId}/`}
+        url={`https://www.instagram.com/lifeatkpn/reel/${postId}/`}
         width="100%"
         className="w-full h-full pointer-events-none"
       />
       {/* Overlay tombol di tengah bawah */}
       <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-0 group-hover:bg-opacity-50 transition">
         <a
-          href={`https://www.instagram.com/p/${postId}/`}
+          href={`https://www.instagram.com/lifeatkpn/reel/${postId}/`}
           target="_blank"
           rel="noopener noreferrer"
-          className="p-2 bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 text-white font-semibold rounded-lg shadow-lg flex items-center gap-2"
+          className="inline-flex items-center gap-2 px-4 py-2 text-white bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 font-semibold rounded-lg"
         >
           <i className="ri-instagram-line text-xl"></i>
-          <span className="text-xs">View on Instagram</span>
+          <span className="text-[10px]">View on Instagram</span>
         </a>
       </div>
     </div>
